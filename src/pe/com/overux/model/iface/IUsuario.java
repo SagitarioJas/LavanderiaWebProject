50dfbc796a108d1fee804d98e7d541e07939b48e
Sergio.Aguirre <1221530@utp.edu.pe>
2017-11-15 14:39:36 -0500

    Hito 5- Service Usuario

--- src/pe/com/overux/model/iface/IUsuario.java
+++ src/pe/com/overux/model/iface/IUsuario.java
@@ -2,9 +2,12 @@ package pe.com.overux.model.iface;
 
 import pe.com.overux.domain.Usuario;
 
+import java.util.ArrayList;
+
 public interface IUsuario {
     public String insert(Usuario user);
     public String update(Usuario user);
     public String delete(Usuario user);
+    public ArrayList<Usuario> listar(Usuario user);
     public Usuario get(Usuario user);
 }
--- src/pe/com/overux/services/iface/IUsuarioService.java
+++ src/pe/com/overux/services/iface/IUsuarioService.java
@@ -2,9 +2,12 @@ package pe.com.overux.services.iface;
 
 import pe.com.overux.domain.Usuario;
 
+import java.util.ArrayList;
+
 public interface IUsuarioService {
     public String insert(Usuario user);
     public String update(Usuario user);
     public String delete(Usuario user);
+    public ArrayList<Usuario> listar(Usuario user);
     public Usuario get(Usuario user);
 }
