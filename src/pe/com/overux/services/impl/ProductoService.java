a367b268585cfc6672bcce4734e8e1e71b57ee00
yomarcs <0821673@utp.edu.pe>
2017-11-18 14:45:22 -0500

    Hito5-ProductService

---
+++ src/pe/com/overux/services/impl/ProductoService.java
@@ -0,0 +1,36 @@
+package pe.com.overux.services.impl;
+
+import pe.com.overux.domain.Producto;
+import pe.com.overux.model.FactoryDao;
+import pe.com.overux.services.iface.IProductoService;
+
+import java.util.ArrayList;
+
+public class ProductoService implements IProductoService {
+    FactoryDao Dao;
+
+    @Override
+    public String insert(Producto clientes) {
+        return null;
+    }
+
+    @Override
+    public String update(Producto clientes) {
+        return null;
+    }
+
+    @Override
+    public String delete(Producto clientes) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Producto> listar(Producto clientes) {
+        return null;
+    }
+
+    @Override
+    public Producto get(Producto clientes) {
+        return null;
+    }
+}
